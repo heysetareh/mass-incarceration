de9c7fa958edd98a7315d6ca8e58284cf6cac2e1
Setareh <setarehbaig@gmail.com>
2017-11-29 08:24:54 -0500

    Update characterpage.js

--- characterpage.js
+++ characterpage.js
@@ -10,12 +10,12 @@ var images = [
   ];
 
 function eddyButton() {
-  	window.location.assign("https://heysetareh.github.io/mass-incarceration/");
+  	window.location.assign("https://heysetareh.github.io/mass-incarceration/eddy.html/");
 
 }
 
 function julesButton() {
-  	window.location.assign("https://heysetareh.github.io/mass-incarceration/");
+  	window.location.assign("https://heysetareh.github.io/mass-incarceration/jules.html/");
 }
 
 function preload() {
